e778f69bd46deba6ce92af42b61aca865e9ef230
ElshadRyanArdiyanto <elshadpw@gmail.com>
2026-07-14 17:18:56 +0700

    push: init types

--- final-project/app/db/models/checkoutModel.ts
+++ final-project/app/db/models/checkoutModel.ts
@@ -1,3 +1,12 @@
-export default class CheckoutModel{
+import { database } from "@/db/config/mongodb";
 
+export default class CheckoutModel{
+    static collection(){
+        return database.collection("users")
+    }
+
+    static async readCheckout(userId: string){
+        const checkout = await this.collection().find({userId: userId}).toArray()
+        return checkout
+    }
 }
