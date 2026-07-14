809a8f884f05168b51ffbf2b098757a952d49793
ElshadRyanArdiyanto <elshadpw@gmail.com>
2026-07-14 17:52:23 +0700

    push: checkout add-get

--- final-project/app/db/models/checkoutModel.ts
+++ final-project/app/db/models/checkoutModel.ts
@@ -1,3 +1,19 @@
+import { Checkout } from "@/app/types";
+import { database } from "../config/mongoDb";
+
 export default class CheckoutModel{
+    static collection(){
+        return database.collection("checkouts")
+    }
+    
+    static async getAllCheckouts(userId: string){
+        const checkouts = await this.collection().find({userId}).toArray()
+        return checkouts
+    }
+
+    static async createCheckout(checkoutData: Checkout, userId: string){
+        const result = await this.collection().insertOne({...checkoutData, userId})
+        return "Checkout created with ID: " + result.insertedId
+    }
 
 }
