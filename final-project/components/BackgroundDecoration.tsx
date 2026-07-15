ecfa48de3d5fcbd7a41db91efa765389833b743e
Latifahtul Khaerani A <khaeranifah@gmail.com>
2026-07-15 10:49:29 +0700

    add wishlist page and its components

---
+++ final-project/components/BackgroundDecoration.tsx
@@ -0,0 +1,32 @@
+import { Sparkles } from "lucide-react";
+
+export default function BackgroundDecoration() {
+  return (
+    <>
+      {/* Top Left */}
+      <div className="absolute left-8 top-44 text-[#F3CDB8] opacity-70">
+        <Sparkles size={36} />
+      </div>
+
+      {/* Top Center */}
+      <div className="absolute left-1/2 top-48 text-[#F3CDB8] opacity-70">
+        <Sparkles size={40} />
+      </div>
+
+      {/* Right */}
+      <div className="absolute right-10 top-40 text-[#F3CDB8] opacity-60">
+        <Sparkles size={30} />
+      </div>
+
+      {/* Bottom Left */}
+      <div className="absolute bottom-72 left-20 text-[#F3CDB8] opacity-60">
+        <Sparkles size={30} />
+      </div>
+
+      {/* Bottom Right */}
+      <div className="absolute bottom-56 right-40 text-[#F3CDB8] opacity-60">
+        <Sparkles size={36} />
+      </div>
+    </>
+  );
+}
