1548d1600d2572236c4a3aa0eae5ba84fb3a62d8
RaihanMufadhal <raihan23rm@gmail.com>
2026-07-16 21:00:39 +0700

    feat: add api for vendor page

---
+++ final-project/app/api/vendor/register/route.ts
@@ -0,0 +1,13 @@
+import UserModel from "@/app/db/models/userModel";
+import errorHandler from "@/app/helpers/errorHandler";
+
+export async function POST(req: Request) {
+  try {
+    const body = await req.json();
+    const result = await UserModel.register(body);
+
+    return Response.json({ message: result }, { status: 201 });
+  } catch (error) {
+    return errorHandler(error);
+  }
+}
--- final-project/app/types.ts
+++ final-project/app/types.ts
@@ -3,141 +3,175 @@ import { ObjectId } from "mongodb";
 // GET INTERFACE
 
 export interface GetUser {
-    _id: string;
-    email: string;
-    password: string;
-    username: string;
+  _id: string;
+  email: string;
+  password: string;
+  username: string;
 }
 
 export interface GetProduct {
-    _id: string;
-    imgUrl: string;
-    desc: string;
-    size: string;
-    theme: string;
-    title: string;
-    OriginalPrice: number;
+  _id: string;
+  imgUrl: string;
+  desc: string;
+  size: string;
+  theme: string;
+  title: string;
+  OriginalPrice: number;
+  vendorId: string;
+  discount: number;
+  finalPrice: number;
+  stock: number;
 }
 
 export interface GetWishlist {
-    _id: string;
-    productId: string;
-    aiImgUrl: string;
-    userId: string;
+  _id: string;
+  productId: string;
+  aiImgUrl: string;
+  userId: string;
 }
 
 export interface GetUserProfile {
-    _id: string;
-    userId: string;
-    address: string;
-    photo: string;
+  _id: string;
+  userId: string;
+  address: string;
+  photo: string;
 }
 
 export interface GetCheckout {
-    _id: string;
-    productId: string;
-    userId: string;
-    status: string;
+  _id: string;
+  productId: string;
+  userId: string;
+  status: string;
 }
 
 export interface GetInputImage {
-    yourImg: File;
-    cosImg: File
+  yourImg: File;
+  cosImg: File;
 }
 
 export interface GetChat {
-    _id: string;
-    content: string;
-    userId: string;
-    roomId: string;
-    imgUrl: string;
+  _id: string;
+  content: string;
+  userId: string;
+  roomId: string;
+  imgUrl: string;
 }
 
 export interface GetRoom {
-    _id: string;
-    nameForum: string;
-    desc: string;
-    img: string;
-    tag: [string];
+  _id: string;
+  nameForum: string;
+  desc: string;
+  img: string;
+  tag: [string];
 }
 
 export interface GetOurEvent {
-    _id: string;
-    eventName: string;
-    category: string;
-    imgUrl: string;
-    forumId: string;
-    description: string;
+  _id: string;
+  eventName: string;
+  category: string;
+  imgUrl: string;
+  forumId: string;
+  description: string;
 }
 
 export interface GetUserDesign {
-    _id: string;
-    imgUrl: string;
-    userId: string;
-    vote: number;
+  _id: string;
+  imgUrl: string;
+  userId: string;
+  vote: number;
+}
+
+export interface GetVendor {
+  _id: string;
+  NamaToko: string;
+  Alamat: string;
+  email: string;
+  password: string;
+  norek: string;
+}
+
+export interface GetMonthlySales {
+  totalSales: number;
+  vendorId: string;
 }
 
 // INI POST INTERFACE
 
 export interface PostUser {
-    email: string;
-    password: string;
-    username: string;
+  email: string;
+  password: string;
+  username: string;
 }
 
 export interface PostProduct {
-    imgUrl: string;
-    desc: string;
-    size: string;
-    theme: string;
-    title: string;
-    OriginalPrice: number;
+  imgUrl: string;
+  desc: string;
+  size: string;
+  theme: string;
+  title: string;
+  OriginalPrice: number;
+  vendorId: string;
+  discount: number;
+  finalPrice: number;
+  stock: number;
 }
 
 export interface PostWishlist {
-    productId: string;
-    aiImgUrl: string;
+  productId: string;
+  aiImgUrl: string;
 }
 
 export interface PostUserProfile {
-    userId: string;
-    address: string;
-    photo: string;
+  userId: string;
+  address: string;
+  photo: string;
 }
 
 export interface PostCheckout {
-    productId: string;
-    status: string;
+  productId: string;
+  status: string;
 }
 
 export interface PostInputImage {
-    yourImg: File;
-    cosImg: File
+  yourImg: File;
+  cosImg: File;
 }
 
 export interface PostChat {
-    content: string;
-    userId: string;
-    roomId: string;
-    imgUrl: string;
+  content: string;
+  userId: string;
+  roomId: string;
+  imgUrl: string;
 }
 
 export interface PostRoom {
-    nameForum: string;
-    desc: string;
-    img: string;
-    tag: [string];
+  nameForum: string;
+  desc: string;
+  img: string;
+  tag: [string];
 }
 
 export interface PostOurEvent {
-    eventName: string;
-    category: string;
-    imgUrl: string;
-    forumId: string;
-    description: string;
+  eventName: string;
+  category: string;
+  imgUrl: string;
+  forumId: string;
+  description: string;
 }
 
 export interface PostUserDesign {
-    imgUrl: string;
-    vote: number;
+  imgUrl: string;
+  vote: number;
+}
+export interface PostVendor {
+  NamaToko: string;
+  Alamat: string;
+  email: string;
+  password: string;
+  norek: string;
+}
+
+export interface PostMonthlySales {
+  totalSales: number;
+  vendorId: string;
 }
