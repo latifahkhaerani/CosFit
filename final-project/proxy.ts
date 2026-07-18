686de2357e7c58cee32ed65905f376e11aaeb2d8
Latifahtul Khaerani A <khaeranifah@gmail.com>
2026-07-18 14:28:47 +0700

    add api/wishlist on proxy

--- final-project/proxy.ts
+++ final-project/proxy.ts
@@ -4,59 +4,62 @@ import { verify } from "jsonwebtoken";
 import { NextResponse } from "next/server";
 
 export async function proxy(request: Request) {
-    try {
-        console.log("Proxy function called");
-        const pathname = new URL(request.url).pathname;
+  try {
+    console.log("Proxy function called");
+    const pathname = new URL(request.url).pathname;
 
-        const cookieStore = await cookies();
-        const authToken = cookieStore.get("Authorization");
+    const cookieStore = await cookies();
+    const authToken = cookieStore.get("Authorization");
 
-        if (!authToken) throw { message: "please login first", status: 401 };
-        const [type, token] = authToken.value.split(" ");
-        if (type !== "Bearer" || !token)
-            throw { message: "please login first", status: 401 };
+    if (!authToken) throw { message: "please login first", status: 401 };
+    const [type, token] = authToken.value.split(" ");
+    if (type !== "Bearer" || !token)
+      throw { message: "please login first", status: 401 };
 
-        const decoded = verify(token, process.env.JWT_SECRET as string) as {
-            id: string;
-            email: string;
-            role: string;
-        };
+    const decoded = verify(token, process.env.JWT_SECRET as string) as {
+      id: string;
+      email: string;
+      role: string;
+    };
 
-        if (pathname.startsWith("/api/vendor")) {
-            if(!pathname.endsWith("/register") || !pathname.endsWith("/login"))
-            {
-                console.log("masuk");
-                if (decoded.role !== "vendor") {
-                    throw {
-                        message: "Forbidden",
-                        status: 403,
-                    };
-                }
-            }
+    if (pathname.startsWith("/api/vendor")) {
+      if (!pathname.endsWith("/register") || !pathname.endsWith("/login")) {
+        console.log("masuk");
+        if (decoded.role !== "vendor") {
+          throw {
+            message: "Forbidden",
+            status: 403,
+          };
         }
-
-        // Clone the request headers and set a new header `x-hello-from-proxy1`
-        const requestHeaders = new Headers(request.headers);
-        requestHeaders.set("x-user-email", decoded.email);
-        requestHeaders.set("x-user-id", decoded.id);
-        requestHeaders.set("x-user-role", decoded.role);
-
-        // You can also set request headers in NextResponse.next
-        const response = NextResponse.next({
-            request: {
-                // New request headers
-                headers: requestHeaders,
-            },
-        });
-
-        return response;
-
-    } catch (err) {
-        return errorHandler(err);
+      }
     }
+
+    // Clone the request headers and set a new header `x-hello-from-proxy1`
+    const requestHeaders = new Headers(request.headers);
+    requestHeaders.set("x-user-email", decoded.email);
+    requestHeaders.set("x-user-id", decoded.id);
+    requestHeaders.set("x-user-role", decoded.role);
+
+    // You can also set request headers in NextResponse.next
+    const response = NextResponse.next({
+      request: {
+        // New request headers
+        headers: requestHeaders,
+      },
+    });
+
+    return response;
+  } catch (err) {
+    return errorHandler(err);
+  }
 }
 
 export const config = {
-    matcher: ["/profile", "/vendor", "/api/vendor", "/api/user/profile"],
+  matcher: [
+    "/profile",
+    "/vendor",
+    "/api/vendor",
+    "/api/user/profile",
+    "/api/user/wishlist",
+  ],
 };
-    
